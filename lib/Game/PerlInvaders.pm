02a91c6a125e0e26858d597d9ab7370734a0613b
Thomas Klausner <domm@cpan.org>
2009-08-05 16:22:30 +0200

    prepared release

---
+++ lib/Game/PerlInvaders.pm
@@ -0,0 +1,40 @@
+package Game::PerlInvaders;
+use 5.010;
+use strict;
+use warnings;
+
+use version 0.77; our $VERSION = qv('0.0.1');
+1;
+
+__END__
+
+=head1 NAME
+
+Game::PerlInvaders - space invaders game implemented using SDL
+
+=head1 SYNOPSIS
+
+  # no usage atm
+
+=head1 DESCRIPTION
+
+A rather simple game of Space Invaders, implemented in a few hours 
+during YAPC::Europe 2009 to get people interested in developing games 
+using Perl and SDL.
+
+As I'm far from a SDL expert, the implementation might suck. If so, 
+patches welcome!
+
+=head1 AUTHOR
+
+Thomas Klausner E<lt>domm {at} cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
